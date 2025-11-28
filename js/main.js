a6ef6b115cada9a2a106eff6b8947d73c24a5775
HyeJi <eoegog@naver.com>
2025-11-28 05:05:15 +0000

    about,skills

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ let greeting = 'Hello, world!';
 
 console.log(greetting);
 
-let nickname = '짐코딩';
-let hobby = ['헬스', '수영'];
+let nickname = '혜지';
+let hobby = ['헬스', '요가'];
 
 let person = {
   nickname: nickname,
